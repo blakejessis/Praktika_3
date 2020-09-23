83533510eb5fb69edbbc68fa05c37541d5dec49a
Timofei Istomin <56717257+blakejessis@users.noreply.github.com>
2020-09-23 19:39:14 +0300

    Create Square.java

---
+++ UML/Square.java
@@ -0,0 +1,32 @@
+public class Square extends Rectangle {
+    public Square(){
+    }
+
+    public Square(double side) {
+    }
+
+    public Square(double side, String color, boolean filled){
+    }
+
+    public double getSide(){
+        return 0;
+    }
+
+    public void setSide(double side){
+    }
+
+    @Override
+    public void setWidth(double width) {
+        super.setWidth(width);
+    }
+
+    @Override
+    public void setLength(double length) {
+        super.setLength(length);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
