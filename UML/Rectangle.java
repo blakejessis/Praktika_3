9dd7d59a8a9ebf081db8f43b27e78002c2864504
Timofei Istomin <56717257+blakejessis@users.noreply.github.com>
2020-09-23 19:38:59 +0300

    Create Rectangle.java

---
+++ UML/Rectangle.java
@@ -0,0 +1,42 @@
+public class Rectangle extends Shape {
+    protected double width;
+    protected double length;
+
+    public Rectangle(){
+    }
+
+    public Rectangle(double width, double length){
+    }
+
+    public Rectangle(double width, double length, String color, boolean filled){
+    }
+
+    public double getWidth() {
+        return 0;
+    }
+
+    public void setWidth(double width) {
+    }
+
+    public double getLength() {
+        return 0;
+    }
+
+    public void setLength(double length) {
+    }
+
+    @Override
+    public double getArea() {
+        return super.getArea();
+    }
+
+    @Override
+    public double getPerimeter() {
+        return super.getPerimeter();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
