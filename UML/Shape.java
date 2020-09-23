991b3dedcc6fb4eb7e1770a668a1757ff743cc90
Timofei Istomin <56717257+blakejessis@users.noreply.github.com>
2020-09-23 19:38:19 +0300

    Create Shape.java

---
+++ UML/Shape.java
@@ -0,0 +1,37 @@
+public abstract class Shape {
+    protected String color;
+    protected boolean filled;
+    public Shape(){
+    }
+
+    public Shape(String color, boolean filled){
+
+    }
+
+    public String getColor(){
+        return null;
+    }
+
+    public void setColor(String color){
+    }
+
+    public boolean isFilled(){
+        return false;
+    }
+
+    public void setFilled(boolean filled){
+    }
+
+    public double getArea(){
+        return 0;
+    }
+
+    public double getPerimeter(){
+        return 0;
+    }
+
+    public String toString(){
+        return null;
+    }
+
+}
