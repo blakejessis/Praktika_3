7656ac0042259479230618a134cb9e5aa107b8c5
Timofei Istomin <56717257+blakejessis@users.noreply.github.com>
2020-09-23 19:38:35 +0300

    Create Circle.java

---
+++ UML/Circle.java
@@ -0,0 +1,34 @@
+public class Circle extends Shape {
+    protected double radius;
+
+    public Circle(){
+    }
+
+    public Circle(double radius){
+    }
+
+    public Circle(double radius, String color, boolean filled){
+    }
+
+    public double getRadius(){
+        return 0;
+    }
+
+    public void setRadius(double radius) {
+    }
+
+    @Override
+    public double getArea() {
+        return super.getArea();
+    }
+
+    @Override
+    public double getPerimeter() {
+        return super.getPerimeter();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
