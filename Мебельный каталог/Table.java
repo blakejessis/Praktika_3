64aa7480893c778533f2c46d33ecf936b6175048
Timofei Istomin <56717257+blakejessis@users.noreply.github.com>
2020-09-24 14:33:53 +0300

    Create Table.java

---
+++ Мебельный каталог/Table.java
@@ -0,0 +1,37 @@
+public class Table extends Furniture {
+    Table(int cost, String maker, String material){
+        this.cost = cost;
+        this.maker = maker;
+        this.material = material;
+    }
+
+    @Override
+    int getCost() {
+        return cost;
+    }
+
+    @Override
+    String getMaker() {
+        return maker;
+    }
+
+    @Override
+    String getMaterial() {
+        return material;
+    }
+
+    @Override
+    void setCost() {
+        this.cost = cost;
+    }
+
+    @Override
+    void setMaker() {
+        this.maker = maker;
+    }
+
+    @Override
+    void setMaterial() {
+        this.material = material;
+    }
+}
