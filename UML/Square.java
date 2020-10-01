0d3c89e347def60997d41d934ced74ea7f06dbf7
Timofei Istomin <56717257+blakejessis@users.noreply.github.com>
2020-10-01 13:56:22 +0300

    Update Square.java

--- UML/Square.java
+++ UML/Square.java
@@ -1,29 +1,31 @@
 public class Square extends Rectangle {
+    private double side;
     public Square(){
-        length = width;
     }
 
     public Square(double side) {
+        setSide(side);
     }
 
     public Square(double side, String color, boolean filled){
     }
 
     public double getSide(){
-        return 0;
+        return side;
     }
 
     public void setSide(double side){
+        this.side = side;
     }
 
     @Override
     public void setWidth(double width) {
-        super.setWidth(width);
+        super.setWidth(side);
     }
 
     @Override
     public void setLength(double length) {
-        super.setLength(length);
+        super.setLength(side);
     }
 
     @Override
