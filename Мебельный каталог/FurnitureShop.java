0e0cf5985befdc96a0e9d38b3ed967df2196dc14
Timofei Istomin <56717257+blakejessis@users.noreply.github.com>
2020-10-01 14:17:44 +0300

    Update FurnitureShop.java

--- Мебельный каталог/FurnitureShop.java
+++ Мебельный каталог/FurnitureShop.java
@@ -13,15 +13,62 @@ public class FurnitureShop {
                 "Диван " + sofa.getCost() + sofa.getMaker() + sofa.getMaterial() + "\n" +
                 "Стол " + table.getCost() + table.getMaker() + table.getMaterial());
 
-
-        String name = in.next();
-
-        if(name.equals("Стул"))
-            System.out.println("Вы приобрели стул");
-        else if(name.equals("Диван"))
-            System.out.println("Вы приобрели диван");
-        else if(name.equals("Стол"))
-            System.out.println("Вы приобрели стол");
-
+        
+        int ind;//переменная для выбора товара
+        
+        
+        System.out.println("Если хотите продолжить, нажмите 1 ");
+        int next = in.nextInt();
+        int price1 = 0,price2 = 0,price3 = 0;
+        int c1 = 0,c2 = 0,c3 = 0;
+        while (next == 1) {
+            System.out.println("Если хотите купить стул(ья) нажмите 1 ");
+            System.out.println("Если хотите купить диван(ы) нажмите 2 ");
+            System.out.println("Если хотите купить стол(ы) нажмите 3 ");
+            ind = in.nextInt();
+            if (ind == 1) {
+                System.out.println("Введите кол-во товара ");
+                c1 = in.nextInt();
+                price1 = c1 * chair.getCost();
+                System.out.println("Если хотите продолжить нажмите 1");
+                next = in.nextInt();
+            }
+            
+            if (ind == 2) {
+                System.out.println("Введите кол-во товара ");
+                c2 = in.nextInt();
+                price2 = c2 * sofa.getCost();
+                System.out.println("Если хотите продолжить нажмите 1");
+                next = in.nextInt();
+            } 
+            if (ind == 3) {
+                System.out.println("Введите кол-во товара ");
+                c3 = in.nextInt();
+                price3 = c3 * table.getCost();
+                System.out.println("Если хотите продолжить нажмите 1");
+                next = in.nextInt();
+            }
+        
+            
+            System.out.println("Ваша покупка: ");
+            
+            if(price1 != 0) {
+                System.out.println("Стол(ы): " + "Кол-во: " + c1 + " Цена: " + price1);
+            }
+            
+            if(price2 != 0) {
+                System.out.println("Шкаф(ы): " + "Кол-во: " + c2 + " Цена: " + price2);
+            }
+            
+            if(price3 != 0) {
+                System.out.println("Стул(ья): " + "Кол-во: " + c3 + " Цена: " + price3);
+            }
+        }
+            
+            
+            System.out.println("Общая сумма заказа: ");
+            int priceAll = price1 + price2 + price3;
+            System.out.println(priceAll);
+       
     }
 }
